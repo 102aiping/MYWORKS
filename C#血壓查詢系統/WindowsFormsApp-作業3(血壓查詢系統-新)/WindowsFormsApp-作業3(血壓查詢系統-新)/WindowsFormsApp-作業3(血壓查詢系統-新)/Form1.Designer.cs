ed3f51a35d35b27af243df51b73540b66b772109
102aiping <42061276+102aiping@users.noreply.github.com>
2018-10-16 17:20:02 +0800

    Add files via upload

---
+++ C#血壓查詢系統/WindowsFormsApp-作業3(血壓查詢系統-新)/WindowsFormsApp-作業3(血壓查詢系統-新)/WindowsFormsApp-作業3(血壓查詢系統-新)/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace WindowsFormsApp_作業3_血壓查詢系統_新_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TB收 = new System.Windows.Forms.TextBox();
+            this.TB舒 = new System.Windows.Forms.TextBox();
+            this.btn送出資料 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(36, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(332, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "iii醫院高血壓查詢系統";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.PaleGreen;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(24, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 40);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "收縮壓";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.PaleGreen;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(24, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 40);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "舒張壓";
+            // 
+            // TB收
+            // 
+            this.TB收.Font = new System.Drawing.Font("微軟正黑體", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.TB收.Location = new System.Drawing.Point(143, 126);
+            this.TB收.Name = "TB收";
+            this.TB收.Size = new System.Drawing.Size(160, 50);
+            this.TB收.TabIndex = 3;
+            this.TB收.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.TB收.TextChanged += new System.EventHandler(this.TB收_TextChanged);
+            // 
+            // TB舒
+            // 
+            this.TB舒.Font = new System.Drawing.Font("微軟正黑體", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.TB舒.Location = new System.Drawing.Point(143, 197);
+            this.TB舒.Name = "TB舒";
+            this.TB舒.Size = new System.Drawing.Size(160, 50);
+            this.TB舒.TabIndex = 4;
+            this.TB舒.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.TB舒.TextChanged += new System.EventHandler(this.TB舒_TextChanged);
+            // 
+            // btn送出資料
+            // 
+            this.btn送出資料.BackColor = System.Drawing.Color.Fuchsia;
+            this.btn送出資料.Font = new System.Drawing.Font("微軟正黑體", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn送出資料.Location = new System.Drawing.Point(114, 293);
+            this.btn送出資料.Name = "btn送出資料";
+            this.btn送出資料.Size = new System.Drawing.Size(189, 57);
+            this.btn送出資料.TabIndex = 5;
+            this.btn送出資料.Text = "送出資料";
+            this.btn送出資料.UseVisualStyleBackColor = false;
+            this.btn送出資料.Click += new System.EventHandler(this.btn送出資料_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.PaleGreen;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(309, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 24);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "mmHg";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.PaleGreen;
+            this.label5.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(309, 152);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 24);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "mmHg";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGreen;
+            this.ClientSize = new System.Drawing.Size(389, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btn送出資料);
+            this.Controls.Add(this.TB舒);
+            this.Controls.Add(this.TB收);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "iii醫院高血壓查詢系統";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TB收;
+        private System.Windows.Forms.TextBox TB舒;
+        private System.Windows.Forms.Button btn送出資料;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ C#血壓查詢系統/WindowsFormsApp-作業3(血壓查詢系統-新)/WindowsFormsApp-作業3(血壓查詢系統-新)/WindowsFormsApp-作業3(血壓查詢系統-新)/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_作業3_血壓查詢系統_新_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TB收_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void TB舒_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn送出資料_Click(object sender, EventArgs e)
+        {
+            string str訊息="";
+            int int收 = 0;
+            int int舒 = 0;
+            
+            if ((TB收.Text!="")&&(TB舒.Text!=""))
+            {
+                bool if收 = false;
+                bool if舒 = false;
+
+                if收 = Int32.TryParse(TB收.Text, out int收);
+                if舒 = Int32.TryParse(TB舒.Text, out int舒);
+
+                if (if收 && if舒)
+                {
+                    int收 = Convert.ToInt32(TB收.Text);
+                    int舒 = Convert.ToInt32(TB舒.Text);
+                    if ((int收>=int舒)&&((int收 <380) && (int收 > 0))&& ((int舒<250) &&(int舒 >0 )))
+                    {
+                        if ((int收 > 60) && (int舒 > 30))
+                        {
+                            if ((int收 > 180) || (int舒 >110))
+                            {
+                                str訊息 = "您的血壓值範圍為:高血壓第三期,請隨時注意血壓";
+                            }
+                            else if ((int收 <= 180) || (int舒 <= 110))
+                            {
+                                str訊息 = "您的血壓值範圍為:高血壓第二期,請經常量測血壓";
+                            }
+                            else if ((int收 < 160) || (int舒 < 100))
+                            {
+                                str訊息 = "您的血壓值範圍為:高血壓第一期,請持續追蹤血壓";
+                            }
+                            else if ((int收 < 140) || (int舒 < 90))
+                            {
+                                str訊息 = "您的血壓值範圍為:正常但是偏高,請定期追蹤血壓";
+                            }
+                            else if ((int收 < 130) || (int舒 < 85))
+                            {
+                                str訊息 = "您的血壓值範圍為:請繼續保持";
+                            }
+                            else if ((int收 < 120) || (int舒 < 80))
+                            {
+                                str訊息 = "您的血壓值範圍為:理想血壓";
+                            }
+                            else
+                            {
+                                str訊息 = "您的血壓值範圍為:理想血壓";
+                            }
+                            str訊息 = "您的收縮壓值為:" + int收.ToString() + "\n您的舒張壓為:" + int舒.ToString()+"\n********************************************\n" + str訊息;
+                            MessageBox.Show(str訊息);
+                            TB收.Text = "";   //自動清空
+                            TB舒.Text = "";   //自動清空
+
+
+                        }
+                        else
+                        {                           
+                            MessageBox.Show(" 您輸入的血壓值偏低,請隨時注意血壓;\n 如輸入有誤請輸入正確血壓值");
+                            TB收.Text = "";   //自動清空
+                            TB舒.Text = "";   //自動清空
+                        }
+                    }
+                    else
+                    {
+                       MessageBox.Show("您輸入的血壓值有誤或不合理, 請輸入正確血壓值");
+                        TB收.Text = "";   //自動清空
+                        TB舒.Text = "";   //自動清空
+                    }
+                }
+                else
+                {
+                  MessageBox.Show("您輸入的血壓值有誤或不合理, 請輸入正確血壓值(收縮壓需大於舒張壓)");
+                    TB收.Text = "";   //自動清空
+                    TB舒.Text = "";   //自動清空
+                }
+            }
+            else
+            {
+                MessageBox.Show("您輸入的血壓值有誤或不合理, 請輸入正確血壓值(不能有空值)");
+                TB收.Text = "";   //自動清空
+                TB舒.Text = "";   //自動清空
+            }
+
+        }
+    }
+}
